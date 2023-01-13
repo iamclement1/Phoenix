1663bf2d6f13cdbbdca4e8095834928ce8c4920d
iamclement1 <blessedmarcel1@gmail.com>
2023-01-13 19:57:57 +0100

    feats: first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold">
+        hello world
+      </h1>
+    </div>
+  )
+}
+
+export default App
