ec193ca4e7a7e448da3908d964b9905ceb78556d
iamclement1 <blessedmarcel1@gmail.com>
2023-01-13 20:11:00 +0100

    feats: file structure created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
+import { Navbar, Footer, Welcome, Services, Transactions } from './components';
 import './App.css'
 
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-4xl font-bold">
-        hello world
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   )
 }
---
+++ client/src/components/index.js
@@ -0,0 +1,10 @@
+//export all components files from a file
+
+export { default as Loader } from './Loader';
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as Transactions } from './Transactions';
+export { default as Services } from './Services';
+export { default as Welcome } from './Welcome';
+
+
