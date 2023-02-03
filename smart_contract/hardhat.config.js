2decf86941957fb787a7d6b39d9e999a2229e50b
iamclement1 <blessedmarcel1@gmail.com>
2023-02-03 11:45:43 +0100

    feats: connected with metamask and set up goerli test network with alchemy

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/A1CoUIQ2KcC0uxvClsx4uOrT6uhCbKgl
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    Goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/A1CoUIQ2KcC0uxvClsx4uOrT6uhCbKgl",
+      accounts: [ 'e6cfc4f6602e956506a953a4555b67bf0fa9f279464aa901874a48b90a63e767' ]
+    }
+  },
 };
